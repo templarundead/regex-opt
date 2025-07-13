c19278fe6b81317a350cbf977258786b8c7b4885
Ghoul <wingoflucifer@me.com>
2025-07-13 16:23:40 +0300

    Minor tweaks

--- main.cc
+++ main.cc
@@ -7,10 +7,9 @@ int main(int argc, const char* const* argv)
     if(argc != 2)
     {
         std::cout
-        << "regex-opt " VERSION " - Copyright (C) 1992,2006 Bisqwit (http://iki.fi/bisqwit/)\n"
-           "This program is distributed under the terms of the General Public License.\n"
-           "\n"
-           "\033[0;38;5;103musage:\033[0m \033[0;38;5;101mregex-opt\033[0m \033[0;38;5;65m<regexp>\033[0m\n";;
+        << "regex-opt " VERSION " — Copyright © 1992, 2006 Bisqwit (http://iki.fi/bisqwit/)\n"
+           "This program is distributed under the terms of the General Public License.\n\n"
+           "\033[0;38;5;103musage:\033[0m \033[0;38;5;65mregex-opt\033[0m \033[0;38;5;101m<regexp>\033[0m\n";
         return 0;
     }
     try {
@@ -24,12 +23,12 @@ int main(int argc, const char* const* argv)
     }
     catch(const char *s)
     {
-        std::cout << "Error: " << s << std::endl;
+        std::cout << "\033[0;38;5;103mError: \033[0m" << s << std::endl;
         return -1;
     }
     catch(const std::string& s)
     {
-        std::cout << "Error: " << s.c_str() << std::endl;
+        std::cout << "\033[0;38;5;103mError: \033[0m" << s.c_str() << std::endl;
         return -1;
     }
     return 0;
